5d2a0055912e965d4a712cc85bdd9eb7a4b80187
ab22 <abelardo2_2@hotmail.com>
2014-01-13 19:05:08 -0600

    Created interfaces and classes for Automapper

---
+++ Dalaran/Infrastructure/AutoMapperWebConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Dalaran.Infrastructure.Interfaces;
+
+namespace Dalaran.Infrastructure
+{
+    public static class AutoMapperWebConfiguration : IGlobalConfiguration
+    {
+        public static void Configure()
+        {
+            /* 
+             * All CreateMaps must go here.
+             * 
+             *  Example:
+             *      Mapper.CreateMap<Products, SearchProductModel>();
+             * 
+             * */
+        }
+    }
+}
---
+++ Dalaran/Infrastructure/Interfaces/IGlobalConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dalaran.Infrastructure.Interfaces
+{
+    public interface IGlobalConfiguration
+    {
+        public static void Configure();
+    }
+}
