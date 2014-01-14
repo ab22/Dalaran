4ebd6f4cee6df0e5b2d6f446d8f8df64ef4b1e9d
Gabriel Recinos <gabrielhh26@gmail.com>
2014-01-14 15:58:01 -0600

    Card 31 - Implementar log4net en la aplicación

--- Dalaran/Startup.cs
+++ Dalaran/Startup.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using log4net;
 
 [assembly: OwinStartupAttribute(typeof(Dalaran.Startup))]
 namespace Dalaran
 {
     public partial class Startup
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Startup));
+        
         public void Configuration(IAppBuilder app)
         {
+            //log4net.Config.BasicConfigurator.Configure();
+
+            log.Debug("This is a debug message");
+            log.Warn("This is a warn message");
+            log.Error("This is a error message");
+            log.Fatal("This is a fatal message");
+
             ConfigureAuth(app);
         }
     }
