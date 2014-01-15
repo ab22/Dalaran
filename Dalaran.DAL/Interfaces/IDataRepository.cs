7d8ced5d1e0b515811314a4c0e5c26b569e962a4
ab22 <abelardo2_2@hotmail.com>
2014-01-14 22:01:16 -0600

    Configured ADO Entities, included AutoFac libraries, and created initial interfaces.

---
+++ Dalaran.DAL/Interfaces/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dalaran.DAL.Interfaces
+{
+    public interface IDataRepository
+    {
+        IQueryable<T> Select<T>(Expression<Func<T, bool>> query) where T: class;
+
+    }
+}
--- Dalaran/Controllers/HomeController.cs
+++ Dalaran/Controllers/HomeController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Dalaran.DAL.Interfaces;
 
 namespace Dalaran.Controllers
 {
     public class HomeController : Controller
     {
-       
+        private IDataRepository repository;
+
+        public HomeController( IDataRepository repository )
+        {
+            this.repository = repository;
+        }
 
         public ActionResult Index()
         {
-            
-            
+            List<string> names = new List<string>();
+            var users = repository.Select<DAL.Users>( x=> x.UserId == 1 );
+
+            foreach (var u in users)
+            {
+                names.Add(u.Username);
+            }
+
+
+            ViewBag.Users = names;
 
             return View();
         }
