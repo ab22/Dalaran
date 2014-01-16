e125f434c96ff57e7965c056d479ff85a86a0881
ab22 <abelardo2_2@hotmail.com>
2014-01-15 21:49:03 -0600

    Removed unused code/classes. Added query example on HomeController

--- Dalaran/Global.asax.cs
+++ Dalaran/Global.asax.cs
@@ -1,14 +1,36 @@
 ﻿using Autofac;
 using Dalaran.Infrastructure;
+using log4net;
+using System;
+using System.Reflection;
+using System.Web;
 
 namespace Dalaran
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private readonly ILog log = LogManager.GetLogger( MethodBase.GetCurrentMethod().DeclaringType );
+
         protected void Application_Start()
         {
             DalaranBootstrapper bootstrapper = new DalaranBootstrapper(new ContainerBuilder());
             bootstrapper.Run();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError().GetBaseException();
+
+            //Do not log 404 exceptions
+            if (ex is HttpException)
+            {
+                if (ex.Message.Contains("The controller for path"))
+                {
+                    return;
+                }
+            }
+
+            log.Fatal(ex.Message);
         }
     }
 }
