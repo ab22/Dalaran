4c55d88c005f821ed5e8fa0bf8837be8955fd63c
ab22 <abelardo2_2@hotmail.com>
2014-01-19 19:02:30 -0600

    Added account state enumerations.

---
+++ Dalaran/Infrastructure/Enumerations/AccountState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Dalaran.Infrastructure.Enumerations
+{
+    public enum AccountState
+    {
+        [Description("Not Validated")]
+        NotValidated,
+        [Description("Active")]
+        Active,
+        [Description("Inactive")]
+        Inactive,
+        [Description("Banned")]
+        Banned
+    }
+}
