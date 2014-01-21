8dea5678d559dc3bf013de1b06c17055986fda7a
Gabriel Recinos <gabrielhh26@gmail.com>
2014-01-21 14:18:45 -0600

    test

---
+++ Dalaran/App/viewmodels/login.js
@@ -0,0 +1,12 @@
+﻿define(['plugins/http', 'knockout', 'jquery'],
+    function (http, ko, $) {
+        var self;
+        var Login = funtion()
+        {
+            self = this;
+        };
+
+        return Login;
+
+    }
+);
