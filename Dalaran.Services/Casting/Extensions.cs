c0a40e40a1dc660030ac64e40932cf6c1591db1b
ab22 <abelardo2_2@hotmail.com>
2014-01-30 19:21:41 -0600

    Added a generic casting global function.

---
+++ Dalaran.Services/Casting/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Dalaran.Services.Casting
+{
+    public static class Extensions
+    {   
+        public static T As<T>(this object o)
+        {
+            return (T)o;
+        }
+    }
+}
