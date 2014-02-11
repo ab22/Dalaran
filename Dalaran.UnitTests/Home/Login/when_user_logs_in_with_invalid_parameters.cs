b935c74b89db77290d1e323f75ba899127ad236d
ab22 <abelardo2_2@hotmail.com>
2014-02-10 21:24:29 -0600

    Extra tests

---
+++ Dalaran.UnitTests/Home/Login/when_user_logs_in_with_invalid_parameters.cs
@@ -0,0 +1,27 @@
+﻿using Machine.Specifications;
+using System;
+
+namespace Dalaran.UnitTests.Home.Login
+{
+    [Subject("Login Validation")]
+    [Tags("Login", "Security")]
+    public class when_user_logs_in_with_invalid_parameters : given_that_a_user_wants_to_authenticate
+    {
+        private static string _veryLongEmail;
+        private static string _veryLongPassword;
+        private static Exception _exception;
+        Establish context =
+            () =>
+            {
+                _veryLongEmail = _veryLongPassword = null;
+            };
+
+        Because of =
+            () => _exception = Catch.Exception(
+                    () => HomeController.Login(_veryLongEmail, _veryLongPassword)
+                );
+
+        It should_throw_an_exception =
+            () => _exception.ShouldBeOfType<ArgumentException>();
+    }
+}
